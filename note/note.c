e7caf2d4203f77ceabf3914baec6157df30aa010
Richard Howe <howe.r.j.89@gmail.com>
2014-10-28 20:09:45 +0000

    updated note

--- note/note.c
+++ note/note.c
@@ -1,47 +1,105 @@
-#include <time.h>
+/*
+ * Small notes program, original from; 
+ *   <http://rosettacode.org/wiki/Take_notes_on_the_command_line#C>
+ *
+ */
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
 
-#define BUF_SZ (512u)
+static char *note_file = ".note/note.log";
 
-char *notefilename = "notes.txt";
-char *usage = "./note <optional string>\n";
+int main(int argc, char **argv)
+{
+        FILE *note = 0;
+        time_t tm;
+        int i;
+        char *p, *homedir;
+        struct passwd *pw = getpwuid(getuid());
 
-FILE *safefopen(char *name, char *mode){
-  FILE *f;
-  f = fopen(name,mode);
-  if(NULL == f){
-    fputs("file open failed\n",stderr);
-    exit(-1);
-  }
-  return f;
+        if (NULL == pw) {
+                perror("getpwuid");
+                return EXIT_FAILURE;
+        }
+
+        homedir = calloc(strlen(pw->pw_dir) + strlen(note_file) + 2, sizeof(char));
+        if (NULL == homedir) {
+                perror("calloc");
+                return EXIT_FAILURE;
+        }
+
+        strcat(homedir, pw->pw_dir);
+        strcat(homedir, "/");
+        strcat(homedir, note_file);
+
+        if (argc < 2) {
+                if ((note = fopen(homedir, "r"))) {
+                        while ((i = fgetc(note)) != EOF)
+                                putchar(i);
+                } else {
+                        perror(homedir);
+                        return EXIT_FAILURE;
+                }
+        } else {
+                if ((note = fopen(homedir, "a"))) {
+                        tm = time(0);
+                        p = ctime(&tm);
+
+                        /* skip the newline */
+                        while (*p)
+                                fputc(*p != '\n' ? *p : '\t', note), p++;
+
+                        for (i = 1; i < argc; i++)
+                                fprintf(note, "%s%c", argv[i], 1 + i - argc ? ' ' : '\n');
+                } else {
+                        perror(homedir);
+                        return EXIT_FAILURE;
+                }
+        }
+
+        if (note)
+                fclose(note);
+        free(homedir);
+
+        return EXIT_SUCCESS;
 }
 
-int main(int argc, char *argv[]){
-  FILE *note;
-  if(argc > 2){ /* too many arguments */
-    fputs(usage,stderr);
-    return EXIT_FAILURE;
-  } else if(argc == 2){ /* note in the form of "note" */
-    time_t time_cur;
-    char *time_str;
-    time_cur = time(NULL);
-    time_str = ctime(&time_cur);
-    note = safefopen(notefilename,"a");
-    fwrite(time_str, sizeof(char), strlen(time_str) - 1, note);
-    fprintf(note," %s\n",argv[1]);
-    fclose(note);
-  } else { /* read note file */
-    char buf[BUF_SZ];
-    int count;
-    note = safefopen(notefilename,"r");
-    do{
-      memset(buf,'\0',BUF_SZ);
-      count = fread(buf, sizeof(char), BUF_SZ, note);
-      puts(buf);
-    } while(BUF_SZ == count);
-    fclose(note);
-  }
-  return EXIT_SUCCESS;
+/* 
+// ORIGINAL ===============================================================
+#include <stdio.h>
+#include <time.h>
+ 
+#define note_file "NOTES.TXT"
+ 
+int main(int argc, char**argv)
+{
+	FILE *note = 0;
+	time_t tm;
+	int i;
+	char *p;
+ 
+	if (argc < 2) {
+		if ((note = fopen(note_file, "r")))
+			while ((i = fgetc(note)) != EOF)
+				putchar(i);
+ 
+	} else if ((note = fopen(note_file, "a"))) {
+		tm = time(0);
+		p = ctime(&tm);
+ 
+		while (*p) fputc(*p != '\n'?*p:'\t', note), p++;
+ 
+		for (i = 1; i < argc; i++)
+			fprintf(note, "%s%c", argv[i], 1 + i - argc ? ' ' : '\n');
+	}
+ 
+	if (note) fclose(note);
+	return 0;
 }
+// ORIGINAL =============================================================== 
+*/
