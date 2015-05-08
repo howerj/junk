018f1f1ee9d81ecaf02f49ddd6946fa070fbfc54
Richard Howe <howe.r.j.89@gmail.com>
2015-05-08 21:10:54 +0100

    updated lcs prog

--- fragments/lcs.c
+++ fragments/lcs.c
@@ -8,70 +8,101 @@
 #include <stdio.h>
 #include <assert.h>
 
-#define MAX(X,Y) ((X) > (Y) ? (X) : (Y))
+#define MAX(X,Y) (((X) > (Y)) ? (X) : (Y))
+
+char *x[] = {
+    "This part of the document has stayed",
+    "the same from version to version.",
+    "",
+    "This paragraph contains text that is",
+    "outdated - it will be deprecated '''and'''",
+    "deleted '''in''' the near future.",
+    "",
+    "It is important to spell check this",
+    "dokument. On the other hand, a misspelled",
+    "word isn't the end of the world.",
+    NULL
+};
+
+char *y[] = {
+    "This is an important notice! It should",
+    "therefore be located at the beginning of",
+    "this document!",
+    "",
+    "This part of the document has stayed",
+    "the same from version to version.",
+    "",
+    "It is important to spell check this",
+    "document. On the other hand, a misspelled",
+    "word isn't the end of the world. This",
+    "paragraph contains important new",
+    "additions to this document.",
+    NULL
+};
 
 typedef struct {
         char *c;
         size_t m, n;
 } diff;
 
-diff *lcs(char *x, char *y)
+diff *lcs(char *x[], char *y[])
 {
         diff *d;
-        size_t m, n, i, j;
         char *c;
+        size_t m=0, n=0, i, j;
         assert(x && y);
-        m = strlen(x);
-        n = strlen(y);
+        while(x[++m]);
+        while(y[++n]);
         if(!(c = calloc((m+1)*(n+1),1)))
                 return perror("calloc failed"), NULL;
         if(!(d = calloc(1, sizeof(*d))))
                 return perror("calloc failed"), NULL;
-        for(i = 1; i < m + 1; i++)
-                for(j = 1; j < n + 1; j++)
-                        if (x[i-1] == y[j-1])
-                                c[(i*m)+j] = c[((i-1)*m)+(j-1)] + 1;
+        for(i = 1; i <= m; i++)
+                for(j = 1; j < n; j++) /*XXX This should be "<="? What did I do?*/
+                        if (!strcmp(x[i-1], y[j-1]))
+                                c[i*n+j] = c[(i-1)*n+(j-1)] + 1;
                         else
-                                c[(i*m)+j] = MAX(c[(i*m)+(j-1)],c[((i-1)*m)+j]);
+                                c[i*n+j] = MAX(c[i*n+(j-1)], c[(i-1)*n+j]);
         d->c = c;
         d->m = m;
         d->n = n;
         return d;
 }
 
-void print_diff_inner(diff *d, char *x, char *y, size_t i, size_t j)
+void print_diff_inner(diff *d, char *x[], char *y[], size_t i, size_t j)
 {
-        if (i > 0 && j > 0 && x[i-1] == y[j-1]) {
+        if (i > 0 && j > 0 && !strcmp(x[i-1], y[j-1])) {
                 print_diff_inner(d, x, y, i-1, j-1);
-                printf("  %c\n", x[i-1]);
+                printf("  %s\n", x[i-1]);
         } else {
-                if (j > 0 && (i == 0 || d->c[(i*(d->m))+(j-1)] >= d->c[((i-1)*(d->m))+j])) {
+                if (j > 0 && (i == 0 || d->c[(i*(d->n))+(j-1)] >= d->c[((i-1)*(d->n))+j])) {
                         print_diff_inner(d, x, y, i, j-1);
-                        printf("+ %c\n", y[j-1]);
-                } else if(i > 0 && (j == 0 || d->c[(i*(d->m))+(j-1)] < d->c[((i-1)*(d->m))+j])) {
-
+                        printf("+ %s\n", y[j-1]);
+                } else if(i > 0 && (j == 0 || d->c[(i*(d->n))+(j-1)] < d->c[((i-1)*(d->n))+j])) {
                         print_diff_inner(d, x, y, i-1, j);
-                        printf("- %c\n", x[i-1]);
+                        printf("- %s\n", x[i-1]);
                 }
         }
-
 }
 
-void print_diff(diff *d, char *x, char *y)
+void print_diff(diff *d, char **x, char **y)
 {
         assert(d && x && y);
         print_diff_inner(d, x, y, d->m, d->n);
 }
 
-int main(int argc, char **argv) 
+int main(void) 
 {
-        diff *c;
-        size_t m,n,i;
-        if(argc != 3)
-                return fprintf(stderr, "usage %s s1 s2\n", argv[0]), -1;
-        printf("%s:%s\n",argv[1], argv[2]);
-        c = lcs(argv[1], argv[2]);
-        print_diff(c, argv[1], argv[2]);
+        size_t i,j;
+        diff *d;
+        d = lcs(x, y);
+        print_diff(d, x, y);
+        for(i = 0; i <= (d->m); i++) {
+                putchar('\n');
+                for(j = 0; j < d->n; j++)
+                        printf("%d ", d->c[i*d->n+j]);
+        }
+        putchar('\n');
         return 0;
 }
 
