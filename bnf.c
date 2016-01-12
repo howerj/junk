b3be80e8963d6121d9867c56a459f92c9e5df5d3
Richard Howe <howe.r.j.89@gmail.com>
2016-01-12 20:53:36 +0000

    Added readme.md and grammar
    Another minor commit

--- bnf.c
+++ bnf.c
@@ -1,3 +1,27 @@
+/** @file       bnf.c
+ *  @brief      A Backus–Naur Form Parser generator for C (work in progress)
+ *  @author     Richard James Howe.
+ *  @copyright  Copyright 2015 Richard James Howe.
+ *  @license    LGPL v2.1 or later version
+ *  @email      howe.r.j.89@gmail.com **/
+#include <stdio.h>
+
+/* Parser specification 
+ * From <https://en.wikipedia.org/wiki/Backus%E2%80%93Naur_Form>
+ * <syntax>         ::= <rule> | <rule> <syntax>
+ * <rule>           ::= <opt-whitespace> "<" <rule-name> ">" <opt-whitespace> "::=" <opt-whitespace> <expression> <line-end>
+ * <opt-whitespace> ::= " " <opt-whitespace> | ""
+ * <expression>     ::= <list> | <list> "|" <expression>
+ * <line-end>       ::= <opt-whitespace> <EOL> | <line-end> <line-end>
+ * <list>           ::= <term> | <term> <opt-whitespace> <list>
+ * <term>           ::= <literal> | "<" <rule-name> ">"
+ * <literal>        ::= '"' <text> '"' | "'" <text> "'" 
+ */
+
+/*parse*/
+
+/*generate C directory or compile to VM, and spit out program and VM? */
+
 int main(void) {
         return 0;
 }
---
+++ readme.md
@@ -0,0 +1,5 @@
+# readme.md
+
+A Backus–Naur Form parser generator and library, which is currently a work in
+progress, this project being a place holder for the idea.
+
